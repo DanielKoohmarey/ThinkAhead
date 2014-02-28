947e9c07e75a2f22c411abf7f9c14687ada203d2
marionhalim <marionhalim@marionhalim-Lenovo-IdeaPad-U310-Touch.(none)>
2014-02-27 23:22:41 -0800

    Cleaned the files and directories

--- thinkahead/wsgi.py
+++ thinkahead/wsgi.py
@@ -1,14 +1,4 @@
-"""
-WSGI config for thinkahead project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thinkahead.settings")
-
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
