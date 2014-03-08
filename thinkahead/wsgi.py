9bfd039193946dda479b6d0ea17711b4fd0d1845
marionhalim <marionhalim@marionhalim-Lenovo-IdeaPad-U310-Touch.(none)>
2014-03-07 17:16:29 -0800

    Editted wsgi.py to work locally

--- thinkahead/wsgi.py
+++ thinkahead/wsgi.py
@@ -1,4 +1,10 @@
+"""
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+"""
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thinkahead.settings")
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
