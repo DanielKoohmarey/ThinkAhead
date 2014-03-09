60f9c65eb212b8495a4fd7ed76829ed7d14fa331
DanielKoohmarey <daniel.koohmarey@berkeley.edu>
2014-03-09 12:59:38 -0700

    added TODOs in view for backend

--- darsplus/views.py
+++ darsplus/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from thinkahead.forms import LoginForm
+from thinkahead.darsplus.models import UserProfile
 from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     """ Load the homepage, or appropriate page depending on user status """
-    if request.user.isauthenticated() and "User has already registered placeholder for db call":
-        return render(request, 'dashboard.html', {'this needs db data':'TODO'})
+    if checkRegistration(request):
+        return render(request, 'dashboard.html', dashBoardData(request.user.username) 
     elif request.user.isauthenticated():
         return render(request, 'register.html', {})
     return render(request, 'home.html', {})
@@ -66,11 +67,23 @@ def dashboard(request):
     """ Button from registration page sends a post request to /dashboard. View takes in post data, populates user profile associated with user, then loads dashboard if no errors on registration page """
     if not request.user.isauthenticated():
         return render(request, 'home.html',RequestContext(request,{}))
-    #TODO: If user has not registered yet (profile does not exist) 
-    if not "User registered placeholder call, probably a get to the user account DB":
+    elif not checkRegistration(request):
         return render(request, 'register.html',RequestContext(request,{}))
     else:
         #TODO: Fill the context dict with all the necessary info, many DB interactions happen here
         #If error creating user profile, render registration.html with errors in context
         return render(request, 'dashboard.html',RequestContext(request,{}))
         
+def checkRegistration(request):
+    """ Check whether or not a user has completed registration """    
+    if request.user.isauthenticated():
+        if UserProfile.UserExists(request.user.username):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def dashboardData(user):
+    """ Retrieve user profile information and return context dictionary for dashboard """
+    pass
