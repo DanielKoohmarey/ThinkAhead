8b08390f1f07299eb334804a31e18b105a19d968
DanielKoohmarey <daniel.koohmarey@berkeley.edu>
2014-03-09 12:47:36 -0700

    cleaning up view logic

--- darsplus/views.py
+++ darsplus/views.py
@@ -4,7 +4,18 @@ from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
+def home(request):
+    """ Load the homepage, or appropriate page depending on user status """
+    if request.user.isauthenticated() and "User has already registered placeholder for db call":
+        return render(request, 'dashboard.html', {'this needs db data':'TODO'})
+    elif request.user.isauthenticated():
+        return render(request, 'register.html', {})
+    return render(request, 'home.html', {})
+
 def userLogin(request):
+    """ View called via post request from button on homepage, attempt to login and load page 
+        depending on registration status """
+
     context = {}
     form = LoginForm(request.POST)
     context['form']=form
@@ -18,10 +29,14 @@ def userLogin(request):
         else:
             return render(request, 'home.html',RequestContext(request,{'errors':"Invalid Username/Password. Please try again."}))
     else:
-        return render(request, 'home.html',RequestContext(request,{'errors':form.errors}))
+        return render(request, 'dashboard.html',RequestContext(request,{'errors':form.errors})
 
-def userCreation(request):
-    context = {}
+def userRegistration(request):
+    """ View called via create user button from homepage, attempts to create user with post data
+    Upon sucesful creation redirects to registration page, else returns to home page"""
+    if not request.user.isauthenticated():
+        return render(request, 'home.html',RequestContext(request,{'errors':"Username/Password cannot be left blank."}))
+context = {}
     form = LoginForm(request.POST)
     context['form']=form
     username, password = request.POST['username'],request.POST['password']
@@ -38,16 +53,24 @@ def userCreation(request):
     new_user.save()
     new_user = authenticate(username=context['username'],password=request.POST['password']) #django requires authentification before logging in
     login(request,new_user)
-    return render(request, 'register.html', RequestContext(request,{}))
-
-
-def userRegistration(request):
-    if not request.user.isauthenticated():
-        return render(request, 'home.html',RequestContext(request,{'errors':"Username/Password cannot be left blank."}))
+    return render(request, 'register.html', RequestContext(request,{}))    
 
 def userLogout(request):
+    """ Logs out current user session if one exists, return to homepage"""
     if request.user.isauthenticated() and 'logout' in request.POST:
         #User was logged in and the logout button was pressed
         logout(request)
     return render(request, 'home.html', RequestContext(request,{}))
+
+def dashboard(request):
+    """ Button from registration page sends a post request to /dashboard. View takes in post data, populates user profile associated with user, then loads dashboard if no errors on registration page """
+    if not request.user.isauthenticated():
+        return render(request, 'home.html',RequestContext(request,{}))
+    #TODO: If user has not registered yet (profile does not exist) 
+    if not "User registered placeholder call, probably a get to the user account DB":
+        return render(request, 'register.html',RequestContext(request,{}))
+    else:
+        #TODO: Fill the context dict with all the necessary info, many DB interactions happen here
+        #If error creating user profile, render registration.html with errors in context
+        return render(request, 'dashboard.html',RequestContext(request,{}))
         
--- thinkahead/urls.py
+++ thinkahead/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from thinkahead.darsplus.views import *
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,7 +9,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^home/$', include(admin.site.urls)),
-    url(r'^registration/$', include(admin.site.urls)),
-    url(r'^dashboard/$', include(admin.site.urls)),
+    url(r'^/$', home),
+    url(r'^login/$', userLogin),
+    url(r'^logout/$', userLogout),
+    url(r'^registration/$',userRegistration),
+    url(r'^dashboard/$', ),
 )
