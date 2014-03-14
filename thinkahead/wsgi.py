efc29cd4a95a54bfcda8014e97cc6c43710b1e68
marionhalim <marionhalim@marionhalim-Lenovo-IdeaPad-U310-Touch.(none)>
2014-03-13 23:51:22 -0700

    HEROKU: Fixed syntax error in wsgi

--- thinkahead/wsgi.py
+++ thinkahead/wsgi.py
@@ -4,12 +4,12 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
-"""
 
+"""
 # Uncomment bottom row for Local server
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thinkahead.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-""""
+"""
