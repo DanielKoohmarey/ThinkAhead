f0859dd44856bc3cbf5525514b037310582709c0
marionhalim <marionhalim@marionhalim-Lenovo-IdeaPad-U310-Touch.(none)>
2014-03-14 04:46:55 -0700

    Reverted to testing local

--- thinkahead/settings.py
+++ thinkahead/settings.py
@@ -108,6 +108,7 @@ FIXTURE_DIRS = (
 The lines that follow configure Heroku settings. Comment out if testing locally
 
 """
+"""
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
@@ -127,3 +128,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+"""
