698712d3f13ba0e11c3ccd0d84f3ba073d06330f
acai <amy.yi.cai@gmail.com>
2014-04-08 04:18:30 -0700

    Fixed drag/drop's shifted active area issue

--- static/dashClient.js
+++ static/dashClient.js
@@ -1,18 +1,20 @@
+/* On page load, run the following code. */
 $(document).ready(function(){
 
 /* 
 Connects individual sortable lists with each other.
 Reference: http://devheart.org/articles/jquery-customizable-layout-using-drag-and-drop/
 */
+var selector;
 for (var i = 0; i < numPlanners; i++) {
-	var selector = '#planner' + (i + 1);
+	selector = '#planner' + (i + 1);
 	$(selector).sortable({
 		connectWith: '.sortableList'
 	});
 }
 
 for (var i = 0; i < numReqs; i++) {
-	var selector = '#req' + (i + 1);
+	selector = '#req' + (i + 1);
 	$(selector).sortable({
 		connectWith: '.sortableList'
 	});
